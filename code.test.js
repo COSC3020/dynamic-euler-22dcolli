e7f4cb762c7e01bae9f00b1994e5ea78ffca58e1
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-05-07 16:32:08 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -4,4 +4,4 @@ eval(fs.readFileSync('code.js')+'');
 
 //assert(factorial(10) === 3628800);
 assert(e(10) === 2.7182818011463845);
-assert(Math.abs(e(5) - Math.exp(5)) < 0.0001);
+assert(Math.abs(e(5) - Math.exp(5)) < 0.01);
